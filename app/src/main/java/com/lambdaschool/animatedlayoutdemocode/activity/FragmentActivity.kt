0013204af1eeda3222c4b06960adae42bfd4d7e1
Chance Payne <chance@lambdaschool.com>
2019-08-20 09:38:39 -0700

    add list fragment in the xml

--- app/src/main/java/com/lambdaschool/animatedlayoutdemocode/activity/FragmentActivity.kt
+++ app/src/main/java/com/lambdaschool/animatedlayoutdemocode/activity/FragmentActivity.kt
@@ -2,9 +2,15 @@ package com.lambdaschool.animatedlayoutdemocode.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.lambdaschool.animatedlayoutdemocode.R
+import com.lambdaschool.animatedlayoutdemocode.fragment.ListFragment
+import com.lambdaschool.animatedlayoutdemocode.model.ShoppingItem
 
-class FragmentActivity : AppCompatActivity() {
+class FragmentActivity : AppCompatActivity(), ListFragment.OnShoppingListFragmentInteractionListener {
+    override fun onFragmentInteraction(item: ShoppingItem) {
+        Toast.makeText(this, "Fragment Interacted ${item.formattedName}", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/lambdaschool/animatedlayoutdemocode/fragment/ListFragment.kt
+++ app/src/main/java/com/lambdaschool/animatedlayoutdemocode/fragment/ListFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import com.lambdaschool.animatedlayoutdemocode.R
+import com.lambdaschool.animatedlayoutdemocode.model.ShoppingItem
 import com.lambdaschool.sprint2_challenge.ItemListAdapter
 import com.lambdaschool.sprint2_challenge.ShoppingItemConstants
 import kotlinx.android.synthetic.main.fragment_list.*
@@ -23,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [ListFragment.OnFragmentInteractionListener] interface
+ * [ListFragment.OnShoppingListFragmentInteractionListener] interface
  * to handle interaction events.
  * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
@@ -33,7 +34,7 @@ class ListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+    private var shoppingListListener: OnShoppingListFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,23 +72,18 @@ class ListFragment : Fragment() {
         list_fragment.adapter = itemListAdapter
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
+        if (context is OnShoppingListFragmentInteractionListener) {
+            shoppingListListener = context
         } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+            throw RuntimeException(context.toString() + " must implement OnShoppingListFragmentInteractionListener")
         }
     }
 
     override fun onDetach() {
         super.onDetach()
-        listener = null
+        shoppingListListener = null
     }
 
     /**
@@ -101,9 +97,8 @@ class ListFragment : Fragment() {
      * (http://developer.android.com/training/basics/fragments/communicating.html)
      * for more information.
      */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+    interface OnShoppingListFragmentInteractionListener {
+        fun onFragmentInteraction(item: ShoppingItem)
     }
 
     companion object {
